fffcb1fab1703c5250286a400621754e038101ce
JavierBoyaca <68155136+bamg359@users.noreply.github.com>
2025-03-07 10:13:00 -0500

    feta se crea modelo usuario

---
+++ src/main/java/com/example/APIClassRoom/ayudas/TipoUsuario.java
@@ -0,0 +1,9 @@
+package com.example.APIClassRoom.ayudas;
+
+public enum TipoUsuario {
+
+    Estudiante,
+    Docente,
+    Administrador
+
+}
---
+++ src/main/java/com/example/APIClassRoom/modelos/Usuario.java
@@ -0,0 +1,77 @@
+package com.example.APIClassRoom.modelos;
+
+import com.example.APIClassRoom.ayudas.TipoUsuario;
+import jakarta.persistence.Entity;
+
+import java.time.LocalDate;
+
+@Entity
+public class Usuario {
+    
+    private Integer id;
+    private String nombre;
+    private String correoElectronico;
+    private String contraseña;
+    private String telefono;
+    private TipoUsuario tipoUsuario;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String nombre, String correoElectronico, String contraseña, String telefono, TipoUsuario tipoUsuario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correoElectronico = correoElectronico;
+        this.contraseña = contraseña;
+        this.telefono = telefono;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+}
