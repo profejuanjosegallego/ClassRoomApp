c3caf1f8cf4fe09a7c075f7d0f531395c9072644
JavierBoyaca <68155136+bamg359@users.noreply.github.com>
2025-03-07 08:13:47 -0500

    feat se crea proyecto base para el api

---
+++ src/main/java/com/example/APIClassRoom/ApiClassRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIClassRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClassRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClassRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/APIClassRoom/ApiClassRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIClassRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClassRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
